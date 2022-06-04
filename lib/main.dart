588f0d5c38767dcfa2b30568e0a42722ef11caad
Administrator <mis93>
2022-06-04 09:53:15 +0600

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: Material(
+          child: Text("Flutter learning within 30 day's"),
+        ),
+      ),
+    );
+  }
+}
